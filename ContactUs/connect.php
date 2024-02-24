743d7e59b8e916472f71edc4f09dd2fe057561ef
jkn1321 <158737467+jkn1321@users.noreply.github.com>
2024-02-24 20:37:32 +0530

    Add files via upload

---
+++ ContactUs/connect.php
@@ -0,0 +1,11 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "contactus";
+
+// Create connection
+$connection = mysqli_connect($servername, $username, $password, $dbname) or die("DB connection failed");
+
+?>
