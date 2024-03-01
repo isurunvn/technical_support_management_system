57348550bcc466279df6bbc2f479e22f8fd15423
Isuru N Liyanaarachchi <isurunaveen27@gmail.com>
2024-03-01 16:54:03 +0530

    version 1.0 update

--- admin/admin.php
+++ admin/admin.php
@@ -28,23 +28,30 @@ if (!isset($_SESSION['username'])) {
         </a>
     </header>
     <main>
-        <section>
-            <h2>Manage Users</h2>
-            <ul>
-                <li><a href="view_users.php"><button>View Users</button></a></li>
-                <li><a href="add_user.php"><button>Add User</button></a></li>
-                <li><a href="search_user.php"><button>Update User</button></a></li>
-                <li><a href="delete_user.php"><button>Delete User</button></a></li>
-            </ul>
-        </section>
-        <section>
-            <h2>Manage Tickets</h2>
-            <ul>
-                <li><button>View Tickets</button></li>
-                <li><button>Assign Tickets</button></li>
-                <li><button>Close Tickets</button></li>
-            </ul>
-        </section>
+        <div class=".container">
+            <div class="bg">
+                <img src="holo.gif">
+            </div>
+            <div class="content">
+                    <section>
+                    <h2>Manage Users</h2>
+                    <ul>
+                        <li><a href="view_users.php"><button>View Users</button></a></li>
+                        <li><a href="add_user.php"><button>Add User</button></a></li>
+                        <li><a href="search_user.php"><button>Update User</button></a></li>
+                        <li><a href="delete_user.php"><button>Delete User</button></a></li>
+                    </ul>
+                </section>
+                <section>
+                    <h2>Manage Tickets</h2>
+                    <ul>
+                        <li><button>View Tickets</button></li>
+                        <li><button>Assign Tickets</button></li>
+                        <li><button>Close Tickets</button></li>
+                    </ul>
+                </section>
+            </div>
+        
     </main>
     <footer>
         <p>&copy; 2024 NexusTech Solutions</p>
